487addc08e42ace95dbebe260a864a005bdd4650
Bharati-Yadav <56573425+Bharati-Yadav@users.noreply.github.com>
2019-10-23 17:49:29 +0530

    check the user is valid or not

---
+++ user.py
@@ -0,0 +1,10 @@
+def username():
+    user=input("Enter the username:")
+   # print(user)
+    def compare():
+        if user=="virat":
+            print("welcome",user)
+        else:
+            print("user not found")
+    compare()
+username()
